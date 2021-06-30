0d14dc9796787e10500d08e3bb722543881e1082
Dhaval Trivedi <dhavaltrivedi00.01@gmail.com>
2021-06-30 21:01:14 +0530

    My Profile Page

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -49,6 +49,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    #Simplified static files serving.
+    #https://warehouse.python.org/project/whitenoise/
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'mywebsite.urls'
